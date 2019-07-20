0e3cc542d6df15e42fa0d9d87bbdc434b9d5f14a
Melody Mastache <16945557+melodymastache@users.noreply.github.com>
2019-07-19 20:51:04 -0400

    Create TesterClass.java

---
+++ workshop8/TesterClass.java
@@ -0,0 +1,10 @@
+package workshop8;
+import javafx.application.Application;
+
+public class TesterClass  {
+	public static void main(String[] args) {
+		Map map = new Map();
+		map.run();
+		Application.launch(Quiz.class);
+	}
+}
